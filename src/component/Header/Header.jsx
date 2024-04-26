d6dbf1856542db616abcb3f440f3ee2898a9afbd
datta-toma <tomadatta15@gmail.com>
2024-04-26 11:37:07 +0600

    navbar and banar

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,42 @@
+import { NavLink } from 'react-router-dom';
+import logo from '../../assets/Design-Artistic-Art-PNG-Photos.png';
+
+const Header = () => {
+
+    const links = <>
+    <li><NavLink to="/" >Home</NavLink></li>
+    <li><NavLink to="/about">Art And Craft</NavLink></li>
+    <li><NavLink to="/update">Add Craft</NavLink></li>
+    <li><NavLink to="/update">Add Craft List</NavLink></li>
+    </>
+
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      {links}
+      </ul>
+    </div>
+    <img className='w-24' src={logo} alt=''></img>
+    <a className="btn btn-ghost text-xl">ARTCRAFT</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+    {links}
+    </ul>
+  </div>
+  <div className="navbar-end">
+  
+  <button className="btn btn-sm  btn-ghost">Login</button>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Root.jsx';
+import ErrorPage from './Pages/ErrorPage.jsx';
+import HomePage from './Pages/HomePage.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children:[
+      {
+        path:"/",
+        element:<HomePage></HomePage>
+      },
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+        <RouterProvider router={router} />
   </React.StrictMode>,
 )
